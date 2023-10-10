9aa6f178fc57554be922ddd179ebbb0397d513bd
Jimmy <toluadejimi@gmail.com>
2023-10-10 11:12:37 +0100

    pushhh

--- app/Console/Commands/SendCron.php
+++ app/Console/Commands/SendCron.php
@@ -116,8 +116,45 @@ class SendCron extends Command
                 PendingTransaction::where('user_id', $user_id)->delete();
 
 
-                $message = "Transaction |  $TransactionReference | NGN $trx->amount | has been sent to VFD ";
+                $curl = curl_init();
+                    $data = array(
 
+                    'ref_trans_id' => $trx->ref_trans_id,
+                    'TransactionReference' => $TransactionReference,
+
+                );
+
+                    $post_data = json_encode($data);
+
+                    curl_setopt_array($curl, array(
+                        CURLOPT_URL => 'https://enkpayapp.enkwave.com/api/pending-transaction',
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => '',
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 0,
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => 'POST',
+                        CURLOPT_POSTFIELDS => $post_data,
+                        CURLOPT_HTTPHEADER => array(
+                            'Content-Type: application/json',
+                        ),
+                    ));
+
+                    $var = curl_exec($curl);
+                    curl_close($curl);
+                    $var = json_decode($var);
+
+
+                    $message = "Transaction reversed | Our API  sent reversal | $error ";
+    
+    
+                    $result = " Message========> " . $message;
+                    send_notification($result);
+
+
+
+                $message = "Transaction |  $TransactionReference | NGN $trx->amount | has been sent to VFD ";
                 send_notification($message);
 
 
@@ -166,10 +203,6 @@ class SendCron extends Command
 
         }
 
-
-
-        $result = " Message========> Working";
-        send_notification($result);
 
 
 
--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('transfer', [TransactionController::class, 'transfer']);
 
-Route::post('test-transaction', [TransactionController::class, 'test']);
 
 
 
